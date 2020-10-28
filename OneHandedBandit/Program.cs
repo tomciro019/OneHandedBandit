c7d4d73d9d715278db3e8af102af176d7a76de2b
Tomáš Cirók <u19.402@pslib.cloud>
2020-10-28 13:18:27 +0100

    Zapisování do programu + výpis

--- OneHandedBandit/Program.cs
+++ OneHandedBandit/Program.cs
@@ -7,6 +7,20 @@ namespace OneHandedBandit
         static void Main(string[] args)
         {
             Bandit bandit = new Bandit(10000);
+            Console.WriteLine("Stav účtu: " + bandit.Wallet);
+            Console.WriteLine("vsazené peníze: ");
+            string convert = Console.ReadLine();
+            int Bet = int.Parse(convert);
+            bool ot = bandit.Verify();
+            if (ot == true)
+            {
+                Console.WriteLine("Vyhrál jsi");
+            }
+            else if (ot == false)
+            {
+                Console.WriteLine("Prohrál jsi");
+            }
+
 
         }
     }
